750ca4be11fa63659d8d31a60604c5a7f072e533
phongva <phongva@newwave.vn>
2025-04-19 12:18:36 +0700

    ff

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import handlebars from 'vite-plugin-handlebars';
-import { resolve } from 'path';
 
 const API_URL = '/';
 
